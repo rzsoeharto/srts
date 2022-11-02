344b8eaa7bb412621d135fdbcd32d557eadf74bb
Davey <64846125+davey-tan@users.noreply.github.com>
2022-11-02 21:49:13 +0700

    test

--- src/components/OfferingSection.jsx
+++ src/components/OfferingSection.jsx
@@ -4,8 +4,29 @@ import { useState } from "react";
 function OfferingSection() {
   const [tab, setTab] = useState();
   return (
-    <div>
-      <div>offeringSection offeringSection</div>
+    <div className="flex flex-row place-content-evenly md:flex-col md:place-content-center">
+      <div className="flex flex-wrap w-1/5 place-content-evenly md:w-3/5">
+        <h1 className="text-md bold">Recycle</h1>
+          <p className="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptate, distinctio veniam impedit facere aut rerum enim dicta
+          </p>
+      </div>
+      <div className="flex flex-wrap w-1/5 place-content-evenly md:w-3/5">
+        <h1 className="text-md bold">Transparency</h1>
+          <p className="text-justify">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non quod
+            accusantium omnis provident voluptatum vel
+          </p>
+      </div>
+      <div className="flex flex-wrap w-1/5 place-content-evenly md:w-3/5">
+        <h1 className="text-md bold">Security</h1>
+          <p className="text-justify">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse at
+            minus sit ipsum aspernatur molestias sint, perspiciatis voluptate
+            fugiat facere!
+          </p>
+      </div>
     </div>
   );
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -50,7 +50,8 @@ function Landing() {
         </div>
 
         {/* ------------------------------------------------------------- */}
-        {/* Our app's foundation */}
+        {/* -------------------------FOUNDATION-------------------------- */}
+        {/* ------------------------------------------------------------- */}
         <div className="flex flex-row place-content-evenly md:flex-col md:place-content-center">
           <div className="flex flex-wrap w-1/5 place-content-evenly md:w-3/5">
             <h1 className="text-md bold">Recycle</h1>
@@ -91,7 +92,10 @@ function Landing() {
             </div>
           </div>
         </div>
-        <OfferingSection />
+        {/* ------------------------------------------------------------- */}
+        {/* -------------------------OFFERING---------------------------- */}
+        {/* ------------------------------------------------------------- */}
+        <OfferingSection/>
       </div>
     </>
   );
