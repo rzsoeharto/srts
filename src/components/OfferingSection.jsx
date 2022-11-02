cff24e2dfb7ae37db8cbbbb082de43e874258527
rzsoeharto <rizkydar13620@gmail.com>
2022-11-02 22:28:33 +0700

    gc dep anjing

--- src/components/OfferingSection.jsx
+++ src/components/OfferingSection.jsx
@@ -5,7 +5,20 @@ function OfferingSection() {
   const [tab, setTab] = useState();
   return (
     <div>
-      <div>offeringSection offeringSection</div>
+      <div className="flex flex-row place-content-center">
+        <div className="flex flex-col w-96 pt-36">
+          <h1 className="landingHeader">Visit our fits page</h1>
+          <p className="protoText text-justify">
+            Visit our fits page and copy a look, or steal the whole fit with a
+            push of a button. Sellers can post an outfit picture with a list of
+            pruducts that’s worn.
+          </p>
+        </div>
+        <img src="../../assets/prototypes/visitPage.png" alt="" />
+      </div>
+      <h1>Don't know what to wear?</h1>
+      <div>offeringSection</div>
+      <div>offeringSection</div>
     </div>
   );
 }
