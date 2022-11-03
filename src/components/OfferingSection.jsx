42376127a1c2fc62000d7a42af117690976e00ab
rzsoeharto <rizkydar13620@gmail.com>
2022-11-03 18:06:22 +0700

    carousell works, no animation yet

--- src/components/OfferingSection.jsx
+++ src/components/OfferingSection.jsx
@@ -1,42 +1,87 @@
 import React from "react";
+import { offers } from ".//offeringData";
 import { useState } from "react";
 
-function OfferingSection() {
-  const [tab, setTab] = useState();
+const OfferingSection = ({ children }) => {
+  const [activeTab, setActiveTab] = useState(1);
+  const offering = offers.find((p) => p.id.toString() === activeTab.toString());
+
+  const handleMin = () => {
+    if (activeTab === 1) {
+      setActiveTab(3);
+    } else {
+      setActiveTab(activeTab - 1);
+    }
+  };
+
+  const handlePlus = () => {
+    if (activeTab !== 3) {
+      setActiveTab(activeTab + 1);
+    } else {
+      setActiveTab(1);
+    }
+  };
+
+  console.log(activeTab);
   return (
-    <div>
-      <div className="flex flex-row place-content-center">
+    <>
+      <div className="flex flex-row place-content-center pt-42">
         <div className="flex flex-col w-96 pt-36">
-          <h1 className="landingHeader">Visit our fits page</h1>
-          <p className="protoText text-justify">
-            Visit our fits page and copy a look, or steal the whole fit with a
-            push of a button. Sellers can post an outfit picture with a list of
-            pruducts that’s worn.
-          </p>
+          <h1 className="landingHeader">{offering.title}</h1>
+          <p className="protoText text-justify">{offering.subText}</p>
         </div>
-        <img src="../../assets/prototypes/visitPage.png" alt="" />
-        <div className="flex flex-row">
-          <div className="flex flex-col space-y-3 pt-64">
-            <h1 className="offersOptions text-end cursor-pointer activeOptions">
+        <img src={offering.img} alt="" className="transition-transform" />
+        <div className="flex flex-row pt-64">
+          <div className="flex flex-col space-y-3">
+            <p
+              className={
+                activeTab === 1
+                  ? "offersOptions text-end cursor-pointer activeOptions"
+                  : "offersOptions text-end cursor-pointer"
+              }
+              onClick={() => setActiveTab(1)}
+            >
               Don't know what to wear?
-            </h1>
-            <h1 className="offersOptions text-end cursor-pointer">
+            </p>
+            <p
+              className={
+                activeTab === 2
+                  ? "offersOptions text-end cursor-pointer activeOptions"
+                  : "offersOptions text-end cursor-pointer"
+              }
+              onClick={() => setActiveTab(2)}
+            >
               Make your money back
-            </h1>
-            <h1 className="offersOptions text-end cursor-pointer">
+            </p>
+            <p
+              className={
+                activeTab === 3
+                  ? "offersOptions text-end cursor-pointer activeOptions"
+                  : "offersOptions text-end cursor-pointer"
+              }
+              onClick={() => setActiveTab(3)}
+            >
               Afraid of scam?
-            </h1>
+            </p>
           </div>
-          <div className="flex flex-col">
-            <img src="../../assets/icons/arrowUp.svg" alt="" />
-            <img src="../../assets/icon/arrowDown.svg" alt="" />
+          <div className="flex flex-col pt-7 space-y-8">
+            <img
+              src="../../assets/icons/arrowUp.svg"
+              className="cursor-pointer"
+              onClick={handleMin}
+              alt=""
+            />
+            <img
+              src="../../assets/icons/arrowDown.svg"
+              className="cursor-pointer"
+              onClick={handlePlus}
+              alt=""
+            />
           </div>
         </div>
       </div>
-      <div>offeringSection</div>
-      <div>offeringSection</div>
-    </div>
+    </>
   );
-}
+};
 
 export default OfferingSection;
