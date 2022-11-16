a7ac4fda446830c691adda41e84304b751a32305
Davey <64846125+davey-tan@users.noreply.github.com>
2022-11-16 10:45:55 +0700

    Fixed DOM property error thing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -63,7 +63,7 @@ function Landing() {
                 className="w-[360px] h-[720px] md:w-[360px]"
                 src="https://www.youtube.com/embed/JUYMC9revJE??autoplay=1&rel=0"
                 title="YouTube video player"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
               ></iframe>
